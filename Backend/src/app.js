83e9dc56c60d596f633ff5c62a266c7900012bc5
DEBOJYOTI BOSE <debojyotibose001@gmail.com>
2026-02-20 19:56:03 +0530

    Production API config

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 // ✅ CORS MUST COME FIRST
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    //  origin: "http://localhost:5173",
     origin: process.env.FRONTEND_URL,
     credentials: true,
   }),
