01b033aebc7a0f67c5c17687749e6c0df6cfdc72
DEBOJYOTI BOSE <debojyotibose001@gmail.com>
2026-02-24 18:25:39 +0530

    axios updated

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -5,14 +5,4 @@ const instance = axios.create({
   withCredentials: true,
 });
 
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      window.location.href = "/user/login";
-    }
-    return Promise.reject(error);
-  },
-);
-
 export default instance;
