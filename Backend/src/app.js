e482bda71896b90c2ff8b0415b5dc4080aafce48
DEBOJYOTI BOSE <debojyotibose001@gmail.com>
2026-02-24 21:43:23 +0530

    app.jsx  updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,25 +1,5 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-
-const router = express.Router();
-const authController = require("../controllers/auth.controller");
-const authMiddleware = require("../middleware/auth.middleware");
-const { logout } = require("../controllers/auth.controller");
-// ================= USER AUTH =================
-
-router.post("/user/register", authController.registerUser);
-router.post("/user/login", authController.LoginUser);
-router.get("/user/logout", authController.LogoutUser);
-
-// ✅ ADD THIS ROUTE
-router.get("/user/me", authMiddleware.authUserMiddleware, (req, res) => {
-  res.status(200).json({
-    user: req.user,
-  });
-});
-
-// ================= FOOD PARTNER AUTH =================
-
 const cors = require("cors");
 
 const app = express();
